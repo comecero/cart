c5f2b65a346924bac79191709e0db5e8179d5cd7
David Halls <dhalls@comecero.com>
2017-11-01 09:51:49 -0600

    Remove deprecated directives from cart and invoice pages, add utility to set api token for use in testing, update output file name for building language template file

---
+++ settings/set_api_token.js
@@ -0,0 +1,35 @@
+function setCookie(name, value, minutes) {
+    if (minutes) {
+        var date = new Date();
+        date.setTime(date.getTime() + (minutes * 60 * 1000));
+        var expires = "; expires=" + date.toUTCString();
+    }
+    else var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(name + "=");
+        if (c_start != -1) {
+            c_start = c_start + name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return null;
+}
+
+$(document).ready(function () {
+
+    $("#submit").click(function () {
+
+        setCookie("token", $("#token").val());
+        document.write("The cookie value for token is: " + getCookie("token"));
+
+    });
+
+});
