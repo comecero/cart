71ada41a6b2bb96dc0f11944f974a57ee90670f1
David Halls <dhalls@comecero.com>
2017-11-13 18:59:59 -0700

    Update changelog, version, copy in latest version of kit

--- dist/js/kit.js
+++ dist/js/kit.js
@@ -1,5 +1,5 @@
 /*
-Comecero Kit version: ﻿1.0.4
+Comecero Kit version: ﻿1.0.5
 https://comecero.com
 https://github.com/comecero/kit
 Copyright Comecero and other contributors. Released under MIT license. See LICENSE for details.
