ca57b1238a726b50091c4f123a773a96391fb8e1
David Halls <dhalls@comecero.com>
2018-01-23 17:02:46 -0700

    Add basic documentation and link builder tool

---
+++ getting-started/app.js
@@ -0,0 +1,32 @@
+﻿var app = angular.module("gettingStarted", ['ngRoute', 'ngSanitize', 'gettext']);
+
+app.config(['$httpProvider', '$routeProvider', '$locationProvider', '$provide', function ($httpProvider, $routeProvider, $locationProvider, $provide) {
+
+    // Define routes
+    $routeProvider.when("/docs", { templateUrl: "../getting-started/docs.html", reloadOnSearch: false, title: "Getting Started - Shopping Cart" });
+    $routeProvider.when("/link-builder", { templateUrl: "../getting-started/link-builder.html", reloadOnSearch: false, title: "Link Builder - Shopping Cart" });
+
+    // Load highlight.js
+    hljs.initHighlightingOnLoad();
+
+    // Set the favicon
+    var faviconUrl = "images/favicon.ico";
+    if (window.__settings.app.favicon_full) {
+        faviconUrl = window.__settings.app.favicon_full;
+    }
+
+    var favicon = document.createElement("link");
+    favicon.setAttribute("rel", "icon");
+    favicon.setAttribute("type", "image/x-icon");
+    favicon.setAttribute("href", faviconUrl);
+    document.head.appendChild(favicon);
+
+}]);
+
+app.run(['$rootScope', function ($rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.$$route.title;
+    });
+}]);
+
+
