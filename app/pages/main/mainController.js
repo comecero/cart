27d0960382ac6162ea84d92c5a8e2611d4143c4b
David Halls <dhalls@comecero.com>
2018-07-11 12:29:44 -0600

    Delete mainController that is no longer required

--- app/pages/main/mainController.js
+++
@@ -1,6 +0,0 @@
-﻿app.controller("MainController", ['$scope', 'SettingsService', 'CurrencyService', function ($scope, SettingsService, CurrencyService) {
-
-    $scope.settings = SettingsService.get();
-    $scope.currency = CurrencyService.getCurrencyName();
-
-}]);
