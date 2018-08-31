44d55f93137949776512d668157d910121418694
David Halls <dhalls@comecero.com>
2018-08-31 10:07:45 -0600

    Add a directive to downoad a receipt

--- app/shared/commonDirectives.js
+++ app/shared/commonDirectives.js
@@ -12,3 +12,24 @@ app.directive('customHtml', function () {
         }
     }
 });
+
+app.directive('downloadReceipt', ['ApiService', function (ApiService) {
+    return {
+        restrict: 'A',
+        scope: {
+            orderId: '=?',
+            orderUrl: '=?',
+            error: '=?'
+        },
+        link: function (scope, elem, attrs) {
+            elem.bind("click", function () {
+                ApiService.getItemPdf(scope.orderUrl).then(function (response) {
+                    var file = new Blob([response.data], { type: "application/pdf" });
+                    saveAs(file, "Order_" + scope.orderId + ".pdf");
+                }, function (error) {
+                    scope.error = error;
+                });
+            });
+        }
+    };
+}]);
